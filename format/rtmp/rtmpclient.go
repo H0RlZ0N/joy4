101e817e7961691e381d493acbf2ec543cee3792
renyaqi <609255356@qq.com>
2022-11-30 11:33:00 +0800

    -fix: 调整sleep时间

--- format/rtmp/rtmpclient.go
+++ format/rtmp/rtmpclient.go
@@ -99,7 +99,7 @@ func (rc *RtmpClient) Serve() {
 			log.Printf("timeout return\n")
 			return
 		default:
-			time.Sleep(time.Nanosecond)
+			time.Sleep(time.Millisecond)
 			var pkt av.Packet
 			pkt, err = rc.demuxer.ReadPacket()
 			if err != nil {
